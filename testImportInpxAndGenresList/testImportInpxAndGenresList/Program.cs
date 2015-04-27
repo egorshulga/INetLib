2fc96ff6a4a14735c8593f6d4245ec3ea68bfce7
Egor Shulga <scrilan@gmail.com>
2015-04-27 12:56:08 +0300

    test app implemented

--- testImportInpxAndGenresList/testImportInpxAndGenresList/Program.cs
+++ testImportInpxAndGenresList/testImportInpxAndGenresList/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace testImportInpxAndGenresList
+﻿using System.Collections.Generic;
+
+namespace testImportInpxAndGenresList
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			GenresList.GenresList.initialize(@"D:\books\_Lib.rus.ec - Официальная\genres_fb2.glst");
 
+			List<BookEntity.BookEntity> db = InpxImport.InpxImport.import(@"D:\books\_Lib.rus.ec - Официальная\librusec_local_fb2.inpx");
+
+			foreach (var book in db)
+				book.printInfoDebug();
 		}
 	}
 }

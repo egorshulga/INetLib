59947360bd03d6c9f53afe2d5887fe6bc1d78ba4
Egor Shulga <scrilan@gmail.com>
2015-06-06 14:27:46 +0300

    fixed: service now prints info via one WriteLine
    multithreading support

--- WCFServiceLibrary/Service.cs
+++ WCFServiceLibrary/Service.cs
@@ -12,64 +12,53 @@ namespace WCFServiceLibrary
 	{
 		public List<BookEntity.BookEntity> selectBooksByAuthor(string authorNameToSearch)
 		{
-			Console.Write("Author query:	{0}. ", authorNameToSearch);
 			var booksList = MetadataDB.MetadataDB.selectBooksByAuthor(authorNameToSearch);
-			Console.WriteLine("Found {0} entities.", booksList.Count);
+			Console.WriteLine("Author query:	{0}. Found {1} entities.", authorNameToSearch, booksList.Count);
 			return booksList;
 		}
 
 		public List<BookEntity.BookEntity> selectBooksByTitle(string titleToSearch)
 		{
-			Console.Write("Title query:	{0}. ", titleToSearch);
 			var books = MetadataDB.MetadataDB.selectBooksByTitle(titleToSearch);
-			Console.WriteLine("Found {0} entities.", books.Count);
+			Console.WriteLine("Title query:	{0}. Found {1} entities.", titleToSearch, books.Count);
 			return books;
 		}
 
 		public List<BookEntity.BookEntity> selectBooksByGenre(int genreIDToSearch)
 		{
-			Console.Write("Genre query:	{0} - {1}. ", GenresList.GenresList.getGenreName(genreIDToSearch), GenresList.GenresList.getGenreDescription(genreIDToSearch));
 			var books = MetadataDB.MetadataDB.selectBooksByGenre(genreIDToSearch);
-			Console.WriteLine("Found {0} entities.", books.Count);
+			Console.WriteLine("Genre query:	{0} - {1}. Found {2} entities.", GenresList.GenresList.getGenreName(genreIDToSearch), GenresList.GenresList.getGenreDescription(genreIDToSearch), books.Count);
 			return books;
 		}
 
 		public List<BookEntity.BookEntity> selectBooksByGenres(Genres genres)
 		{
-			Console.Write("Genres query:	{0}. ", genres.getGenres());
 			var books = MetadataDB.MetadataDB.selectBooksByGenres(genres);
-			Console.WriteLine("Found {0} entities.", books.Count);
+			Console.WriteLine("Genres query:	{0}. Found {1} entities.", genres.getGenres(), books.Count);
 			return books;
 		}
 
 		public BookEntity.BookEntity selectBookByID(int bookID)
 		{
-			Console.Write("Book ID query:	");
 			var book = MetadataDB.MetadataDB.selectBookByID(bookID);
-			book.printInfoDebug();
+			Console.Write("Book ID query:	{0}", book);
 			return book;
 		}
 
 		public List<BookEntity.BookEntity> selectBooksByTemplate(BookEntity.BookEntity template)
 		{
-//			Console.Write("Template query:	{0} - {1} - {2}. ", template.authors[0].fullName, template.title, template.genres);
-			Console.Write("Template query:	");
-			Console.Write(string.IsNullOrEmpty(template.authors[0].fullName) ? "[all]" : template.authors[0].fullName);
-			Console.Write(" - ");
-			Console.Write(string.IsNullOrEmpty(template.title) ? "[all]" : template.title);
-			Console.Write(" - ");
-			Console.Write(template.genres.genresIDs.Count == 0 ? "[all]" : template.genres.ToString());
-			Console.Write(". ");
-
 			var books = MetadataDB.MetadataDB.selectBooksByTemplate(template);
-			Console.WriteLine("Found {0} entities.", books.Count);
+			Console.WriteLine("Template query:	{0} - {1} - {2}. Found {3} entities. ",
+				string.IsNullOrEmpty(template.authors[0].fullName) ? "[all]" : template.authors[0].fullName,
+				string.IsNullOrEmpty(template.title) ? "[all]" : template.title,
+				template.genres.genresIDs.Count == 0 ? "[all]" : template.genres.ToString(), 
+				books.Count);
 			return books;
 		}
 
 		public Stream extractBook(BookEntity.BookEntity book)
 		{
-			Console.Write("Book query:	");
-			book.printInfoDebug();
+			Console.Write("Book query:	{0}", book);
 			return BookExtractor.BookExtractor.extract(book);
 		}
 
